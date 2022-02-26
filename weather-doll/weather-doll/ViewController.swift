e94c855e249f3b65d25cb2dd048f205715fbbdb6
parkikbum <pibum@naver.com>
2022-02-26 14:48:26 +0900

    [ADD] 프로젝트 구축

---
+++ weather-doll/weather-doll/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  weather-doll
+//
+//  Created by 박익범 on 2022/02/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
