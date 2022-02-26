586a2fbf1b0278e604694af42a24a953802b5617
parkikbum <pibum@naver.com>
2022-02-26 15:18:58 +0900

    [FEAT] delete StoryBoards for project

---
+++ weather-doll/weather-doll/Source/Extensions/Date+.swift
@@ -0,0 +1,8 @@
+//
+//  Date+.swift
+//  weather-doll
+//
+//  Created by 박익범 on 2022/02/26.
+//
+
+import Foundation
---
+++ weather-doll/weather-doll/Source/Vcs/Home/HomeVC.swift
@@ -0,0 +1,30 @@
+//
+//  HomeVC.swift
+//  weather-doll
+//
+//  Created by 박익범 on 2022/02/26.
+//
+
+import UIKit
+
+class HomeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .orange
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
