26f065c525fcf589a3211fc1de1ada0e6dda1171
parkikbum <pibum@naver.com>
2022-02-26 21:06:17 +0900

    [ADD] add UILabel extension (set AttributeText)

---
+++ weather-doll/weather-doll/Source/Components/WeatherDiscriptionView.swift
@@ -0,0 +1,13 @@
+//
+//  WeatherDiscription.swift
+//  weather-doll
+//
+//  Created by 박익범 on 2022/02/26.
+//
+
+import Foundation
+import UIKit
+
+class WeatherDiscriptionView: UIView() {
+    
+}
--- weather-doll/weather-doll/Source/Extensions/UILabel+.swift
+++ weather-doll/weather-doll/Source/Extensions/UILabel+.swift
@@ -5,4 +5,32 @@
 //  Created by 박익범 on 2022/02/26.
 //
 
-import Foundation
+import UIKit
+
+extension UILabel {
+    func setBoldAttributeText(targetString: [String], font: UIFont) {
+        var rangeArr: [NSRange] = []
+        var index = 0
+        let text = self.text ?? ""
+        var range = NSRange(location: 0, length: text.count)
+
+        let attributeString = NSMutableAttributedString(string: text)
+        
+        while (range.location != NSNotFound) {
+            if (range.length == 0) {
+                break
+            }
+            range = (attributeString.string as NSString).range(of: targetString[index], range: range)
+            rangeArr.append(range)
+            if (range.location != NSNotFound) {
+                index += 1
+                range = NSRange(location: range.location + range.length, length: text.count - (range.location + range.length))
+            }
+        }
+        rangeArr.forEach { (range) in
+            attributeString.addAttribute(.font, value: font, range: range)
+            print(range)
+        }
+        self.attributedText = attributeString
+    }
+}
