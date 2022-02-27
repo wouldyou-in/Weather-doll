7b48ee3a7298bf5751c177d4b62d900f1a03ec21
parkikbum <pibum@naver.com>
2022-02-28 01:41:13 +0900

    [ADD] 바텀시트 뷰 서치바 컴포넌트 추가

--- weather-doll/weather-doll/Source/Components/BottomSheet/SearchBar.swift
+++ weather-doll/weather-doll/Source/Components/BottomSheet/SearchBar.swift
@@ -5,4 +5,31 @@
 //  Created by 박익범 on 2022/02/28.
 //
 
-import Foundation
+import UIKit
+import SnapKit
+import Then
+
+class SearchBar: UITextField {
+    func setLayout(){
+        self.backgroundColor = UIColor.subGrayColor
+        self.tintColor = UIColor.black
+        self.font = UIFont.gmarketSansMediumFont(ofSize: 24)
+        self.textColor = UIColor.searchTextColor
+        self.addLeftPadding(9)
+        self.layer.cornerRadius = 10
+        self.clipsToBounds = true
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder: ) has not been implemented")
+    }
+    
+    init(){
+        super.init(frame: .zero)
+        setLayout()
+    }
+}
