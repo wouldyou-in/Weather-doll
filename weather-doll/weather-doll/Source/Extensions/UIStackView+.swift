b672f92eebaec792c32ab1a7fd85e1317eeeb20f
parkikbum <pibum@naver.com>
2022-03-02 01:26:38 +0900

    [ADD] 스택뷰 익스텐션 추가

---
+++ weather-doll/weather-doll/Source/Extensions/UIStackView+.swift
@@ -0,0 +1,8 @@
+//
+//  UIStackView+.swift
+//  weather-doll
+//
+//  Created by 박익범 on 2022/03/01.
+//
+
+import Foundation
---
+++ weather-doll/weather-doll/Source/Models/CellModel.swift
@@ -0,0 +1,8 @@
+//
+//  CellModel.swift
+//  weather-doll
+//
+//  Created by 박익범 on 2022/03/01.
+//
+
+import Foundation
