76fcde02da2bce11d55f7bd59c4523659e0cee5b
parkikbum <pibum@naver.com>
2022-03-02 01:27:09 +0900

    [ADD] stackView 익스텐션 추가

--- weather-doll/weather-doll/Source/Extensions/UIStackView+.swift
+++ weather-doll/weather-doll/Source/Extensions/UIStackView+.swift
@@ -5,4 +5,10 @@
 //  Created by 박익범 on 2022/03/01.
 //
 
-import Foundation
+import UIKit
+
+extension UIStackView {
+    func addArrangedSubviews(_ views: [UIView]) {
+        views.forEach{ addArrangedSubview($0) }
+    }
+}
